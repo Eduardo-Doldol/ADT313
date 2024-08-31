299ea44ca06f2211eda5226f0e44094894b837d6
Eduardo Doldol <thirdydoldol02@gmail.com>
2024-08-31 17:43:10 +0800

    hands on 1

---
+++ hands-on1-3b/src/component/User/User.jsx
@@ -0,0 +1,11 @@
+function User() {
+  return (
+    <div>
+      <h1>Eduardo Doldol</h1>
+      <h2>BSIT-3B</h2>
+      <p>Just reading if I have a little bit free time.</p>
+    </div>
+  )
+}
+
+export default User;
